894d66f92efbd71937b1f40a580a0a77df48690a
Samuel <ssitero@knights.ucf.edu>
2018-10-30 22:53:46 -0400

    d

--- Space_Shooter/Assets/Scripts/GameController.cs
+++ Space_Shooter/Assets/Scripts/GameController.cs
@@ -1,55 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
-
+public class GameController : MonoBehaviour
+{
     public GameObject hazard;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
-   // private bool GameOver;
-   // private bool Restart;
 
     public Text ScoreText;
     private int score;
 
-    public Text RestartText;
-  
-   
-
-    // Use this for initialization
-    void Start () {
-
-      
-       
+    void Start()
+    {
         score = 0;
         UpdateScore();
-        StartCoroutine (SpawnWaves());
-	}
-
- 
+        StartCoroutine(SpawnWaves());
+    }
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-
-        while (true){
+        while (true)
+        {
             for (int i = 0; i < hazardCount; i++)
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate (hazard, spawnPosition, spawnRotation);
+                Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
-
-           
         }
     }
+
     public void AddScore(int newScoreValue)
     {
         score += newScoreValue;
@@ -60,7 +47,4 @@ public class GameController : MonoBehaviour {
     {
         ScoreText.text = "Score: " + score;
     }
-
-   
 }
-
